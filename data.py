dd0f0b3ab55a18a92696c7477b0af7a8ea5831d6
shayestehhamid <shayesteh.hamid@gmail.com>
2017-04-11 17:58:27 +0430

    no chart, works compelte

--- data.py
+++ data.py
@@ -1,25 +1,37 @@
 import xlrd
 
+n_o_times = 36
+n_o_places = 20
+
 class Course:
-	"""docstring for Course"""
 	def __init__(self, courseID, courseProf, courseStudents):
 		self.courseID = courseID
 		self.courseProf = courseProf
 		self.courseStudents = courseStudents
 
+class Place:
+	def __init__(self, placeID, capacity):
+		self.placeID = placeID
+		capacity = capacity
 
 class Data:
 
 	def open_file(self, path):
-		"""
-		Open and read an Excel file
-		"""
 		book = xlrd.open_workbook(path)
 		self.coursesNum = book.nsheets
+		self.coursesNames = book.sheet_names()
+		
 		for i in range(self.coursesNum):
 			thisSheet = book.sheet_by_index(i)		
 			#print thisSheet.col_values(0)
-			self.courses.append(Course(i, "", thisSheet.col_values(0)))
+			cs = thisSheet.col_values(0)
+			self.courses.append(Course(i, "", cs))
+			for st in cs:
+				try:
+					self.students[int(st)].append(i)
+				except:
+					self.students[int(st)] = [i]
+		# print self.students
 
 	def calculateConflicts(self):
 		def hasConflict(c1, c2):
@@ -34,27 +46,49 @@ class Data:
 	def calculateTR45(self, cost):
 		return [[self.conflicts[i][j]*cost for j in range(self.coursesNum)] for i in range(self.coursesNum)]
 
-	def calculateTR2(self, minCost, maxCost):
-		costPlus = (maxCost-minCost)*1.0/self.coursesNum
-		def helper():
-			return True
-		#a = [maxCost-c for c]
-		return [[]]
+	def calculateTR2(self, minCost, maxCost, timeNum):
+		
+		costPlus = (maxCost-minCost)*1.0/timeNum
+		tmp = []
+		for i in range(timeNum):
+			tmp.append(maxCost-(i*costPlus))
+
+		return [tmp for i in xrange(self.coursesNum)]
+
+	def calculateTR6(self):
+		"""course-place"""
+		return [ [True for i in range(n_o_places)] for j in self.courses]
+
+	def calculateTR1(self):
+		"""time-place"""
+		return [ [True for i in range(n_o_places)] for j in range(n_o_times)]
+
+	def getCourseName(self, index):
+		return self.coursesNames[index]
+
+	def getTime(self, index):
+		return str(index/4 + 13), str((index%4)*3 + 8)
+
+	def getStudentCourses(self, st):
+		""" st: student ID (String or int) """
+		return self.students[int(st)]
+
+	def getCourseIntersections(self, c1, c2):
+		return self.courses[c1].intersection(self.courses[c1])
 
 	def __init__(self, path):
+		self.students = {}
 		self.courses = []
 		self.open_file(path)
 		self.conflicts = self.calculateConflicts()
 		self.TR4 = self.calculateTR45(100*1000)
-		self.TR5 = self.calculateTR45(1000*1000)
-		self.TR2 = self.calculateTR2(10*1000)
+		self.TR5 = self.calculateTR45(5000*1000)
+		# print 'start4', self.TR4[0], 'end'
+		# print 'start5', self.TR5[0], 'end'
+		self.TR2 = self.calculateTR2(7*1000, 10*1000, n_o_times)
+		self.places = []
+		self.TR1 = self.calculateTR1()
+		self.TR6 = self.calculateTR6()
+		self.n_o_times = 40
 
-
-
-
-a = Data("Book1.xlsx")
-print a.conflicts[5][4]
-print a.TR4[5][4]
-print a.TR5[5][4]
-
-
+#a = Data("Book1.xlsx")
--- optimizer.py
+++ optimizer.py
@@ -1,41 +1,49 @@
 from pulp import *
 import data
+import matplotlib.pyplot as plt
+
 data = data.Data("Book1.xlsx")
 
 problem = LpProblem('Project', LpMinimize)
 
-n_o_places = 10
-n_o_time = 10
+n_o_places = 20
+n_o_time = 36
 
 
+import time
 
-
+start_time = time.time()
 ########################
+
 # TV2
 # course * course
-var_in_day_binary = [[] _ for i in xrange(data.coursesNum)]
+var_in_day_binary = [[] for _ in xrange(data.coursesNum)]
 for i in xrange(data.coursesNum):
 	for j in xrange(data.coursesNum):
 		name = ",".join(["TV2cc", str(i), str(j)])
 		var = LpVariable(name, 0, 1, LpInteger)
 		var_in_day_binary[i].append(var)
+
 # TV3
 # course * course
-var_continues = [[] _ for i in xrange(data.coursesNum)]
-for i in xrange(data.coursesNum):
-	for j in xrange(data.coursesNum):
-		name = ",".join(["TV3cc", str(i), str(j)])
-		var = LpVariable(name, 0, 1, LpInteger)
-		var_continues[i].append(var)
+
+
+# var_continues = [[] for i in xrange(data.coursesNum)]
+# for i in xrange(data.coursesNum):
+# 	for j in xrange(data.coursesNum):
+# 		name = ",".join(["TV3cc", str(i), str(j)])
+# 		var = LpVariable(name, 0, 1, LpInteger)
+# 		var_continues[i].append(var)
+
+
 # TV4
 #course * time * places
-var_cr_time_places = [[[] for j in xrange(n_o_places)] for _ in xrange(data.coursesNum)]
+var_cr_time = [[] for _ in xrange(data.coursesNum)]
 for i in xrange(data.coursesNum):
-	for j in xrange(n_o_places):
 		for k in xrange(n_o_time):
-			name = ','.join(["TV4cpt", str(i), str(j), str(k)]) # course, place, time
+			name = ','.join(["TV4cpt", str(i), str(k)]) # course, place, time
 			var = LpVariable(name, 0, 1, LpInteger)
-			var_cr_time_places[i][j].append(var)
+			var_cr_time[i].append(var)
 ###########################
 
 # const 2
@@ -44,75 +52,218 @@ for i in xrange(data.coursesNum):
 for c1 in xrange(data.coursesNum):
 	for c2 in xrange(data.coursesNum):
 		if data.conflicts[c1][c2]:
-			
+			if c1 == c2:
+				continue
 			for time in xrange(n_o_time):
 				const = []
-				for place in xrange(n_o_places):
-					const.append(var_cr_time_places[c1][place][time])
-					const.append(var_cr_time_places[c2][place][time])
+				
+				const.append(var_cr_time[c1][time])
+				const.append(var_cr_time[c2][time])
 				problem += lpSum(const) <= 1
 
 # const 3
 # constrains for AA table, being after each other!
+times_serial = []
+for time1 in xrange(n_o_time):
+	for time2 in xrange(n_o_time):
+		if time1/4 == time2/4 and abs(time1 - time2) == 1:
+			times_serial.append((time1, time2))
+
+
 for c1 in xrange(data.coursesNum):
 	for c2 in xrange(data.coursesNum):
 		if data.conflicts[c1][c2]:			
-			for time in xrange(n_o_time):
+			for t1, t2 in times_serial:
 				const = []
-				for place in xrange(n_o_places):
-					const.append(var_cr_time_places[c1][place][time])
-					const.append(var_cr_time_places[c2][place][time])
-				problem += lpSum(const) <= 2*var_continues[c1][c2]
+			
+				const.append(var_cr_time[c1][t1])
+				const.append(var_cr_time[c2][t2])
+				problem += lpSum(const) <= 1 
 
 # const 4
 # constrains for AD table, being in one day
+times_day = []
+for time1 in xrange(n_o_time):
+	for time2 in xrange(n_o_time):
+		if time1/4 == time2/4 and abs(time1 - time2) > 1:
+			times_day.append((time1, time2))
+
 for c1 in xrange(data.coursesNum):
 	for c2 in xrange(data.coursesNum):
 		if data.conflicts[c1][c2]:			
-			for time in xrange(n_o_time):
+			for t1, t2 in times_day:
 				const = []
-				for place in xrange(n_o_places):
-					const.append(var_cr_time_places[c1][place][time])
-					const.append(var_cr_time_places[c2][place][time])
-				problem += lpSum(const) <= 2*var_in_day_binary[c1][c2]
+				
+				const.append(var_cr_time[c1][t1])
+				const.append(var_cr_time[c2][t2])
+				problem += lpSum(const) <= var_in_day_binary[c1][c2] + 1
 
 # const 5
 # sigma on time, sigma on places, for a specific course = 1, get sure time and room has been assigned
 for course in xrange(data.coursesNum):
 	const = []
-	for time in xrange(n_o_time):
-		for place in xrange(n_o_places):
-			const.append(var_cr_time_places[course][place][time])
-	problem += lpSum(const) = 1
+	for time in xrange(n_o_time):	
+		const.append(var_cr_time[course][time])
+	problem += lpSum(const) == 1
 
 # const 6
 # get sure the place is free in the time
 # not exists for now!!!
 # love our department!
 
+#get sure there is a place assign!
+# get sure they do not have intersection one place, need an permission variable,.
+
 # const 7
 # in a place, a time, only one exam is held ( or less )
-for time in xrange(n_o_time):
-	for place in xrange(n_o_places):
-		const = []
-		for course in xrange(data.coursesNum):
-			const.append(var_cr_time_places[course][place][time])
-		problem += lpSum(const) <= 1
 
+
+# solve this by permission!
+
+#
+const = []
+for c1 in xrange(data.coursesNum):
+	for c2 in xrange(data.coursesNum):
+		if c1 < c2:
+			const.append(var_in_day_binary[c1][c2]*data.conflicts[c1][c2])
+problem += lpSum(const) < 50
 
 ############################
 #objective!
-for course in xrange(data.coursesNum):
-	for course in xrange(data.coursesNum):
-		problem += var_in_day_binary[course][course]*data.TR4[course][course]
-		problem += var_continues[course][course]*data.TR5[course][course]
+for course1 in xrange(data.coursesNum):
+	for course2 in xrange(data.coursesNum):
+		if course1 != course2:
+			problem += var_in_day_binary[course1][course2]*data.TR4[course1][course2]
+			# problem += var_continues[course1][course2]*data.TR5[course1][course2]
 # zarb in_day ba element moshabeh
 # zarb continues ba element moshabeh
+for course in xrange(data.coursesNum):
+	for time in xrange(n_o_time):
+			problem += var_cr_time[course][time]*data.TR2[course][time]
 # zarb coursetime, dar hanizeie time
 
 ########################
 
 
+print "problem created!"
+status = problem.solve()
+print LpStatus[status]
+print "problem solved!"
 
-problem.solve()
 
+import xlsxwriter
+workbook = xlsxwriter.Workbook('res1.xlsx')
+worksheet = workbook.add_worksheet()
+
+
+
+
+
+result= []
+
+# problem.writeLP("problem.lp")
+row = 0
+for course in xrange(data.coursesNum):
+	for time in xrange(n_o_time):
+		if value(var_cr_time[course][time]) == 1:
+			worksheet.write(row, 0, data.getCourseName(course))
+			worksheet.write(row, 1, data.getTime(time)[0])
+			worksheet.write(row, 2, data.getTime(time)[1])
+			result.append((course, time))
+			row += 1
+
+workbook.close()
+
+n_days_conf = 0
+n_serial_conf = 0
+two_serial_day = 0
+
+for index1 in xrange(len(result)):
+	for index2 in xrange(len(result)):
+		if index1 < index2:
+			c1, t1 = result[index1]
+			c2, t2 = result[index2]
+			if abs((t1/4)-(t2/4)) == 1:
+				two_serial_day += data.conflicts[c1][c2]
+			if t1/4 == t2/4 and abs(t1-t2)==1:
+				n_serial_conf += data.conflicts[c1][c2]
+				continue
+			elif t1/4 == t2/4:
+				n_days_conf += data.conflicts[c1][c2]
+				continue
+
+print "male ma bashe"
+print n_days_conf, "day conf"
+print n_serial_conf, "serial conf"
+print two_serial_day, "two serial days!"
+
+
+
+
+# import networkx as nx
+# G = nx.Graph()
+
+
+# for i in xrange(data.coursesNum):
+# 	G.add_node(i)
+
+
+# for index1 in xrange(data.coursesNum):
+# 	for index2 in xrange(data.coursesNum):
+# 		if index1 < index2 and data.conflicts[index1][index2]:
+# 			G.add_edge(index1, index2, weight=data.conflicts[index1][index2])
+
+
+
+# pos = nx.circular_layout(G)
+# nx.draw_networkx(G, pos)
+
+# labels = {}
+# for i in xrange(data.coursesNum):
+# 	labels[i] = str(G.degree(i))+","+data.getCourseName(i)
+
+# nx.draw_networkx_labels(G, pos, labels)
+
+# plt.figure(1, figsize=(1000, 1000))
+# plt.axis('off')
+# plt.savefig("weighted_graph.png") # save as png
+# plt.show() # displa
+
+
+
+
+# import xlrd
+
+# book = xlrd.open_workbook('Book3.xlsx')
+# sheet = book.sheet_by_index(0)
+
+# course = sheet.col_values(0)
+# course = [int(x) for x in course]
+# times = sheet.col_values(2)
+# for index in xrange(len(times)):
+# 	t1, t2 = times[index].split('-')
+# 	print int(t1), int(t2)
+
+# 	times[index] = int(t1)*3 + int(t2)
+
+# result1 = zip(course, times)
+
+# for index1 in xrange(len(result1)):
+# 	for index2 in xrange(len(result1)):
+# 		if index1 < index2:
+# 			c1, t1 = result1[index1]
+# 			c2, t2 = result1[index2]
+# 			if t1/3 == t2/3 and abs(t1-t2)==1:
+# 				n_serial_conf += data.conflicts[c1][c2]
+# 				continue
+# 			if t1/3 == t2/3:
+# 				n_days_conf += data.conflicts[c1][c2]
+# 				continue
+
+# print "male ouna bashe"
+# print n_serial_conf
+# print n_days_conf
+
+import time
+finish_time = time.time()
+print "time: ", finish_time - start_time
